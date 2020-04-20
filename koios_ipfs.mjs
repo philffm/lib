ab35fa56afcf8f822eaafbde0beb4f34499d6e4f
CryingEagle1 <31490364+CryingEagle1@users.noreply.github.com>
2020-04-21 00:41:40 +0200

    Added subtitle to Ipfs functionality [needs structure and download fix]

--- koios_ipfs.mjs
+++ koios_ipfs.mjs
@@ -1,23 +1,28 @@
 //console.log(`In ${window.location.href} starting script: ${import.meta.url}`);
-import {forIPFSexport} from './koios_youtube.mjs';
-import {loadScriptAsync} from './koios_util.mjs';
+import {
+  forIPFSexport
+} from './koios_youtube.mjs';
+import {
+  loadScriptAsync
+} from './koios_util.mjs';
 
 
-export async function setupIPFS() {
+export async function setupIPFS()
+{
   console.log("In SetupIPFS");
-  await Promise.all( [ // see https://www.npmjs.com/package/ipfs
-      loadScriptAsync("https://unpkg.com/ipfs/dist/index.js"),
-      ]);
-   console.log("Ipfs libraries loaded");
-   var ipfs = await window.Ipfs.create(); //await??
-   return ipfs;
+  await Promise.all([ // see https://www.npmjs.com/package/ipfs
+    loadScriptAsync("https://unpkg.com/ipfs/dist/index.js"),
+  ]);
+  console.log("Ipfs libraries loaded");
+  var ipfs = await window.Ipfs.create(); //await??
+  return ipfs;
 }
 
-export async function uploadYtToIpfs()
+export async function uploadYtDataToIpfs()
 {
   var ipfs = await setupIPFS();
-  var hash;                   //IPFS hash
-  for await (const result of ipfs.add(JSON.stringify(await forIPFSexport())))
+  var hash; //IPFS hash
+  for await (const result of ipfs.add(JSON.stringify(await includeSubtitlesforIpfsExport())))
   {
     console.log(result);
     hash = result.path;
@@ -37,8 +42,78 @@ export async function getYtInfoIpfs(hash)
   return videoAndPlaylistInfo;
 }
 
-export async function uploadSubtitlesIpfs(subtitleData)
+export async function includeSubtitlesforIpfsExport()
 {
-  console.log("in upload IPFS subtitles!!!!");
-  console.log(subtitleData);
+  var info = await forIPFSexport();
+  for(var i = 0; i<info.length;i++)
+  {
+    for(var x = 0; x<info[i].videos.length;x++)
+    {
+      info[i].videos[x].subtitles = await getSubTitles(info[i].videos[x].videoid);
+    }
+  }
+  return info;
+}
+
+
+
+
+
+
+
+var parser = new DOMParser();
+
+export async function getYouTubeSubTitle(language, videoId)
+{
+  var array = [];
+  var subtitleUrl = `https://video.google.com/timedtext?v=${videoId}&lang=${language}`;
+  var data = await fetch(subtitleUrl).catch(console.log);
+  var t = await data.text();
+  var captions = parser.parseFromString(t, "text/html").getElementsByTagName('text');
+  for (var i = 0; i < captions.length; i++)
+  {
+    var s = captions[i].innerHTML;
+    s = s.replace(/&amp;/g, "&");
+    s = s.replace(/&quot;/g, "'");
+    s = s.replace(/&#39;/g, "'");
+    array.push({
+      start: captions[i].getAttribute('start'),
+      dur: captions[i].getAttribute('dur'),
+      text: s
+    });
+  }
+  return array;
+}
+
+
+export async function getSubtitleList(videoId)
+{
+  var subtitleUrl = `https://video.google.com/timedtext?type=list&v=${videoId}`;
+  var data = await fetch(subtitleUrl).catch(console.log);
+  var t = await data.text();
+  var subtitleList = parser.parseFromString(t, "text/xml").getElementsByTagName('track');
+  console.log(subtitleList);
+  return subtitleList;
+}
+
+
+export async function getSubTitles(videoId)
+{
+  var captions = await getSubtitleList(videoId);
+  var allVidSubs = [];
+  for (var i=0; i<captions.length; i++){
+    var language = captions[i].getAttribute('lang_code');
+    console.log(`Found language: ${language}`);
+    allVidSubs.push({
+      lang: language,
+      subtitle: await getYouTubeSubTitle(language, videoId)
+    });
+    /*if (language != "vor")
+    { // reserved for slide info
+      var arraypromise = getYouTubeSubTitle();
+    }
+    */
+  }
+  console.log(allVidSubs);
+  return allVidSubs;
 }
--- koios_youtube.mjs
+++ koios_youtube.mjs
@@ -16,23 +16,23 @@ loadScriptAsync("https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.
 // See https://developers.google.com/youtube/v3/docs/playlists/list?apix=true
 
 export async function GetYouTubePlaylists() {
-    
+
     await LoadGapi();
     var list=await gapi.client.youtube.playlists.list({
       "part": "snippet", // contentDetails
       "channelId": "UCMyWjw6D7eq6swaOljtwJdw" // koios online channel
     });
-    
+
     var resultlist=[]
     for (var i=0;i<list.result.items.length;i++) {
         var result={};
         result.id    = list.result.items[i].id;
         result.title = list.result.items[i].snippet.title;
         result.description = list.result.items[i].snippet.description;
-        
-        
-        
-        
+
+
+
+
         result.thumbnail = list.result.items[i].snippet.thumbnails.high.url; // default.url;
         resultlist.push(result)
     }
@@ -41,19 +41,19 @@ export async function GetYouTubePlaylists() {
     return resultlist;
 }
 
-export async function GetYouTubePlayListItems() {  
+export async function GetYouTubePlayListItems(_playlistId) {
 
     const queryString = window.location.search;
     //console.log(`In GetYouTubePlayListItems queryString=${queryString}`);
 
     const urlParams = new URLSearchParams(queryString);
     //console.log(urlParams);
-    
-    let playlistId = urlParams.get('playlistId') || "PL_tbH3aD86KvXkp5y0eB85_GEze1gBsKD";
+
+    let playlistId = urlParams.get('playlistId') || _playlistId;
     // koios intro PL_tbH3aD86KvXkp5y0eB85_GEze1gBsKD
     // level 2 "PL_tbH3aD86Kt7mITDw67sJMI6M0fRF2Zx";
     // level 1 "PL_tbH3aD86Kt-vJy4Q-rvZtXDmrLMG1Ef";
-    
+
 
     console.log(`playlistId=${playlistId}`);
 
@@ -62,7 +62,7 @@ export async function GetYouTubePlayListItems() {
     var resultlist=[]
     await LoadGapi();
     do {
-        var list=await gapi.client.youtube.playlistItems.list({ 
+        var list=await gapi.client.youtube.playlistItems.list({
           "part": "snippet", // contentDetails
           "playlistId": playlistId,
           "maxResults": 50,
@@ -73,37 +73,37 @@ export async function GetYouTubePlayListItems() {
         //console.log(`GetYouTubePlayListItems: next ${list.result.items.length} records`);
         //console.log(nextPageToken);
         var idlist="";
-        
+
         var resultlistindex=resultlist.length;
         for (var i=0;i<list.result.items.length;i++) {
             var snippet=list.result.items[i].snippet;
-            var result={};            
+            var result={};
             var deslines = snippet.description.split("\n"); // find ___ChapterTitles___
-            if (deslines[0] && deslines[0].includes("___")) {  
+            if (deslines[0] && deslines[0].includes("___")) {
                result.title   = deslines[0].replace(/_/g,"");
                result.chapter = true;
                resultlist.push(result)
             }
-                    
+
             result={};
             idlist +=(idlist?",":"")+snippet.resourceId.videoId;
             //result.id    = list.result.items[i].id;
             result.title        = snippet.title;
             result.description  = snippet.description;
-            
+
             console.log(snippet.thumbnails);
-            
+
             result.thumbnail    = snippet.thumbnails.maxres? snippet.thumbnails.maxres.url : snippet.thumbnails.high.url; // default.url;
             result.videoid      = snippet.resourceId.videoId
             result.chapter      = false;
             resultlist.push(result);
         }
-                
+
         var list2=await gapi.client.youtube.videos.list({
             "part": "contentDetails",
             "id": idlist
         });
-        
+
         //console.log(resultlistindex);
         for (var i=0;i<list2.result.items.length;i++) {
             while (resultlist[resultlistindex].chapter) // skip the chapters
@@ -111,10 +111,10 @@ export async function GetYouTubePlayListItems() {
             var contentDetails=list2.result.items[i].contentDetails;
             resultlist[resultlistindex].duration = moment.duration(contentDetails.duration).asSeconds();
             resultlistindex++
-        }        
+        }
     } while (nextPageToken);
     //console.log(resultlist);
-    return resultlist;    
+    return resultlist;
 }
 
 export async function forIPFSexport()     //creates an array of objects in the right format for IPFS export.
