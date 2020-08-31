1ce4143f0d3fc8bd7a5e651351d185e990a122f3
Corwin <corwinvandalen@gmail.com>
2020-08-31 12:56:28 +0200

    Changed split to comma

--- leaderboard_transfer/Transfertokens/koiosf_transfertokens.mjs
+++ leaderboard_transfer/Transfertokens/koiosf_transfertokens.mjs
@@ -24,7 +24,7 @@ async function onLoad() {
 
 async function AddElementsToList() {
     var addresslist=getElement("addresstextboxtext").innerHTML; 
-    useraddresses = addresslist.split(' ');
+    useraddresses = addresslist.split(',');
     ShowAddresses(useraddresses);
 }
 
