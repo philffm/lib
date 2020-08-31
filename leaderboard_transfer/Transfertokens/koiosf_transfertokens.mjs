37820d05602223876818ef95c9a48881989368fb
Corwin <corwinvandalen@gmail.com>
2020-08-31 12:18:08 +0200

    Update

--- leaderboard_transfer/Transfertokens/koiosf_transfertokens.mjs
+++ leaderboard_transfer/Transfertokens/koiosf_transfertokens.mjs
@@ -2,18 +2,18 @@ import {DomList,getElement,FitOneLine} from '../../lib/koiosf_util.mjs';
 
 let useraddresses;
 let tokenamount;
-var AddressList = new DomList("transfertokensentry");
+var GlobalAddressList = new DomList("transfertokensentry");
 
 onLoad();
 
 async function onLoad() {
-    var addresslist=getElement("addresstextboxtext")    
+    var addresslist=getElement("addresstextboxtext");    
     addresslist.contentEditable="true"; // make div editable
     addresslist.style.whiteSpace ="pre"; 
     if(addresslist.innerHTML == "Insert here...")
         addresslist.addEventListener("click", addresslist.innerHTML(""));
 
-    var tokenamountlist=getElement("tokenamounttextboxtext")    
+    var tokenamountlist=getElement("tokenamounttextboxtext");    
     tokenamountlist.contentEditable="true"; // make div editable
     tokenamountlist.style.whiteSpace ="pre"; 
     if(tokenamountlist.innerHTML == "Insert here...")
