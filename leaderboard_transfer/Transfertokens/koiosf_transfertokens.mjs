8dca5fb755f0ad95896a3aa8e5bc82124b86ec3b
Corwin <corwinvandalen@gmail.com>
2020-08-31 14:01:51 +0200

    Included names

--- leaderboard_transfer/Transfertokens/koiosf_transfertokens.mjs
+++ leaderboard_transfer/Transfertokens/koiosf_transfertokens.mjs
@@ -2,6 +2,7 @@ import {DomList,getElement,FitOneLine} from '../../lib/koiosf_util.mjs';
 
 let useraddresses;
 let tokenamount;
+let usernames;
 var GlobalAddressList = new DomList("transfertokensentry");
 
 onLoad();
@@ -10,30 +11,33 @@ async function onLoad() {
     var addresslist=getElement("addresstextboxtext");    
     addresslist.contentEditable="true"; // make div editable
     addresslist.style.whiteSpace ="pre"; 
-    if(addresslist.innerHTML == "Insert here...")
-        addresslist.addEventListener("click", addresslist.innerHTML(""));
 
     var tokenamountlist=getElement("tokenamounttextboxtext");    
     tokenamountlist.contentEditable="true"; // make div editable
     tokenamountlist.style.whiteSpace ="pre"; 
-    if(tokenamountlist.innerHTML == "Insert here...")
-        tokenamountlist.addEventListener("click", tokenamountlist.innerHTML(""));
+    
+    var tokenamountlist=getElement("namestextboxtext");    
+    tokenamountlist.contentEditable="true"; // make div editable
+    tokenamountlist.style.whiteSpace ="pre"; 
 
     getElement("confirmbutton").addEventListener('animatedclick',AddElementsToList)   
     getElement("emptylistbutton").addEventListener('animatedclick',EmptyList)   
 }
 
 async function AddElementsToList() {
+    var nameslist=getElement("namestextboxtext").innerHTML; 
+    usernames = nameslist.split(',');
     var addresslist=getElement("addresstextboxtext").innerHTML; 
     useraddresses = addresslist.split(',');
     var tokenlist=getElement("tokenamounttextboxtext").innerHTML; 
     tokenamount = tokenlist.split(',');
-    ShowAddresses(useraddresses,tokenamount);
+    ShowAddresses(nameslist,useraddresses,tokenamount);
 }
 
-async function ShowAddresses(addresses,tokenamount) {
+async function ShowAddresses(nameslist,addresses,tokenamount) {
     for (var i=0;i<addresses.length;i++) {
         var target = GlobalAddressList.AddListItem();
+        target.getElementsByClassName("transferusernametext")[0].innerHTML = nameslist[i];
         target.getElementsByClassName("transferuseraddresstext")[0].innerHTML = addresses[i];
         target.getElementsByClassName("transfertokencounttext")[0].innerHTML = tokenamount[i];
         var tokenamountlist=getElement("transfertokencounttext",target);    
