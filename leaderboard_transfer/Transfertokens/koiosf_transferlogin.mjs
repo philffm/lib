ad1c9f16929d6b388c72a2f79d52874f3d5e4753
Corwin <corwinvandalen@gmail.com>
2020-09-01 13:27:05 +0200

    Removed elements depending on UI

--- leaderboard_transfer/Transfertokens/koiosf_transferlogin.mjs
+++ leaderboard_transfer/Transfertokens/koiosf_transferlogin.mjs
@@ -1,22 +1,6 @@
 import {loadScriptAsync,getElement,GetImageIPFS,publish,setElementVal} from '../../lib/koiosf_util.mjs';
 import { } from "../../lib/3box.js"; // from "https://unpkg.com/3box/dist/3box.js"; // prevent rate errors
 
-/*
-
-const infuraKey = "37a4c5643fe0470c944325f1e9e12d50";
-var providerOptions = {
-/*    walletconnect: {
-        package: WalletConnectProvider, // required
-        options: {
-            infuraId: infuraKey // required
-        }
-    }
-    
-};
-
-*/
-
-
 let web3Modal     // Web3modal instance
 var  provider;  // Chosen wallet provider given by the dialog window
 let selectedAccount;     // Address of the selected account
@@ -33,11 +17,7 @@ async function init() {
         await loadScriptAsync("https://unpkg.com/@walletconnect/web3-provider@latest/dist/umd/index.min.js"),
         await loadScriptAsync("https://cdn.jsdelivr.net/npm/fortmatic@latest/dist/fortmatic.js"),        // https://unpkg.com/fortmatic@2.0.6/dist/fortmatic.js
         ])
-    console.log("After promise all");
-	
-	
-	
-	
+    console.log("After promise all");	
 }    
 
 if (window.ethereum)
@@ -45,18 +25,11 @@ if (window.ethereum)
     else
         window.ethereum=1; // so at least a box is shown
 
-
-
-
-
 window.addEventListener('DOMContentLoaded', asyncloaded);  // load  
  
-
 function ClearCachedProvider() {
     web3Modal.clearCachedProvider();
 }
-
-
 
 async function asyncloaded() {  
 console.log("asyncloaded login")
@@ -131,9 +104,6 @@ export function getUserAddress() {
 
 }
 
-
-
-
 export async function Login() {
     await onConnect();    
 }
@@ -141,110 +111,6 @@ export async function Login() {
 export async function authorize() {
     await onConnect();
 }    
-
-
-/**
- * Kick in the UI action after Web3modal dialog has chosen a provider
- */
-async function fetchAccountData() {
-
-  // Get a Web3 instance for the wallet
-  
-
-
-
-  // Get connected chain id from Ethereum node
-  const chainId = await web3.eth.getChainId();
-  
-  console.log(`Chain id=${chainId}`)
-  // Load chain information over an HTTP API
-  var chainData=`Chain: ${chainId}`
-  try {
-     chainData = (await EvmChains.getChain(chainId)).name;    
-  } catch(err) { console.log(err); } // but still continue
-  
-  getElement("chain").textContent = chainData;
-  
-
-  // Get list of accounts of the connected wallet
-  const accounts = await web3.eth.getAccounts();
-
-  // MetaMask does not give you all accounts, only the selected account
-  console.log("Got accounts", accounts);
-  selectedAccount = accounts[0];
-
-  getElement("account").textContent = selectedAccount;
-
-
-// Read profile data
-const profile = await Box.getProfile(selectedAccount)
-console.log(profile)
-if (profile) {
-    getElement("name").textContent = `${profile.name?profile.name:"No name defined yet on 3box"} ${profile.emoji?profile.emoji:""}`
-    if (profile.image) {
-        var imagecid=profile.image[0].contentUrl
-        imagecid=imagecid[`\/`]
-        console.log(imagecid);
-        getElement("userphoto ").src=await GetImageIPFS(imagecid)
-    }
-}    
-
-  // Get a handl
- //const template = document.querySelector("#template-balance");
-  //const accountContainer = document.querySelector("#accounts");
-
-  // Purge UI elements any previously loaded accounts
-  //accountContainer.innerHTML = '';
-
-  // Go through all accounts and get their ETH balance
-  const rowResolvers = accounts.map(async (address) => {
-    const balance = await web3.eth.getBalance(address);
-    // ethBalance is a BigNumber instance
-    // https://github.com/indutny/bn.js/
-    const ethBalance = web3.utils.fromWei(balance, "ether");
-    const humanFriendlyBalance = parseFloat(ethBalance).toFixed(4);
-    // Fill in the templated row and put in the document
-    //const clone = template.content.cloneNode(true);
-    //clone.querySelector(".address").textContent = address;
-    //clone.querySelector(".balance").textContent = humanFriendlyBalance;
-    //accountContainer.appendChild(clone);
-  });
-
-  // Because rendering account does its own RPC commucation
-  // with Ethereum node, we do not want to display any results
-  // until data for all accounts is loaded
-  await Promise.all(rowResolvers);
-
-  // Display fully loaded UI for wallet data
-  //document.querySelector("#prepare").style.display = "none";
-  //document.querySelector("#connected").style.display = "block";
-}
-
-
-
-/**
- * Fetch account data for UI when
- * - User switches accounts in wallet
- * - User switches networks in wallet
- * - User connects wallet initially
- */
-async function refreshAccountData() {
-
-  // If any current data is displayed when
-  // the user is switching acounts in the wallet
-  // immediate hide this data
- //.document.querySelector("#connected").style.display = "none";
-  //document.querySelector("#prepare").style.display = "block";
-
-  // Disable button while UI is loading.
-  // fetchAccountData() will take a while as it communicates
-  // with Ethereum node via JSON-RPC and loads chain data
-  // over an API call.
- // document.querySelector("#btn-connect").setAttribute("disabled", "disabled")
-  await fetchAccountData(provider);
-  //document.querySelector("#btn-connect").removeAttribute("disabled")
-}
-
 
 /**
  * Connect wallet button pressed.
@@ -305,32 +171,6 @@ async function onConnect() {
 
 console.log(provider);
   
-}
-
-/**
- * Disconnect wallet button pressed.
- */
-async function onDisconnect() {
-
-  console.log("Killing the wallet connection", provider);
-
-  // TODO: Which providers have close method?
-  if(provider.close) {
-    await provider.close();
-
-    // If the cached provider is not cleared,
-    // WalletConnect will default to the existing session
-    // and does not allow to re-scan the QR code with a new wallet.
-    // Depending on your use case you may want or want not his behavir.
-    await web3Modal.clearCachedProvider();
-    provider = null;
-  }
-
-  selectedAccount = null;
-
-  // Set the UI back to the initial state
- // document.querySelector("#prepare").style.display = "block";
-  //document.querySelector("#connected").style.display = "none";
 }
 
 
