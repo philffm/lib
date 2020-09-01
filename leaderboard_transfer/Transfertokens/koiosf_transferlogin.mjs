5c7e25cb7c28ffca6dd839216c6b2539a5d0294c
Corwin <corwinvandalen@gmail.com>
2020-09-01 13:31:25 +0200

    Simple web3

--- leaderboard_transfer/Transfertokens/koiosf_transferlogin.mjs
+++ leaderboard_transfer/Transfertokens/koiosf_transferlogin.mjs
@@ -1,169 +1,60 @@
-import {loadScriptAsync,getElement,GetImageIPFS,publish,setElementVal} from '../../lib/koiosf_util.mjs';
-import { } from "../../lib/3box.js"; // from "https://unpkg.com/3box/dist/3box.js"; // prevent rate errors
+import {}  from "https://unpkg.com/web3@latest/dist/web3.min.js"; // {Web3}
 
-let web3Modal     // Web3modal instance
-var  provider;  // Chosen wallet provider given by the dialog window
-let selectedAccount;     // Address of the selected account
-var web3;
+let web3;
+let currentChainId;
+let currentUserAddress;
 
-var initpromise=init();
+export async function authorize() {
+  // Check if metamask or other is installed
+  if (typeof window.ethereum !== undefined) {
+    try {
+      // Ask user to allow usage of ethereum account
+      await window.ethereum.enable()
+    } catch {
+      window.alert("Without Metamask this is not going to work");
+      return
+    }
 
-async function init() {
-     await Promise.all(
-        [
-        await loadScriptAsync("https://unpkg.com/web3@latest/dist/web3.min.js"),
-        await loadScriptAsync("https://unpkg.com/web3modal"),        
-        await loadScriptAsync("https://unpkg.com/evm-chains/lib/index.js"),        
-        await loadScriptAsync("https://unpkg.com/@walletconnect/web3-provider@latest/dist/umd/index.min.js"),
-        await loadScriptAsync("https://cdn.jsdelivr.net/npm/fortmatic@latest/dist/fortmatic.js"),        // https://unpkg.com/fortmatic@2.0.6/dist/fortmatic.js
-        ])
-    console.log("After promise all");	
-}    
+    // Setup listeners for chain or account changes
+    window.ethereum.on('chainIdChanged', (id) => {setChainId(id)});
+    window.ethereum.on('accountsChanged', (address) => {setUserAddress(address[0])});
 
-if (window.ethereum)
-    window.ethereum.autoRefreshOnNetworkChange=false; // prevent autoreload
-    else
-        window.ethereum=1; // so at least a box is shown
+    // Disable refresh on network change, will be default in future
+    window.ethereum.autoRefreshOnNetworkChange = false;
 
-window.addEventListener('DOMContentLoaded', asyncloaded);  // load  
- 
-function ClearCachedProvider() {
-    web3Modal.clearCachedProvider();
+    return await initializeWeb3();
+  } else {
+    // Show error when no metamask is found
+    window.alert("Metamask should be installed to use this page!");
+  }
 }
 
-async function asyncloaded() {  
-console.log("asyncloaded login")
-//console.log(getElement("login"))
-//console.log(getElement("login_comment"))   
-
-    console.log("login");
-    await initpromise;
-   
-
-    const Web3Modal = window.Web3Modal.default;
-    const WalletConnectProvider = window.WalletConnectProvider.default;
-    const EvmChains = window.EvmChains;
-    const Fortmatic = window.Fortmatic;
-
-
-
-    console.log("Initializing example");
-    console.log("WalletConnectProvider is", WalletConnectProvider);
-    console.log("Fortmatic is", Fortmatic);
-
-      // Tell Web3modal what providers we have available.
-      // Built-in web browser provider (only one can exist as a time)
-      // like MetaMask, Brave or Opera is added automatically by Web3modal
-      const providerOptions = {
-        walletconnect: {
-          package: WalletConnectProvider,
-          options: {
-            // Mikko's test key - don't copy as your mileage may vary
-            infuraId: "8043bb2cf99347b1bfadfb233c5325c0",
-          }
-        },
-
-        fortmatic: {
-          package: Fortmatic,
-          options: {            
-            key: "pk_test_C80030486E9F6B17"
-          }
-        }
-      };
-      
-      
-console.log("web3Modal");      
-
-      web3Modal = new Web3Modal({
-        cacheProvider: true, // remember previousely selected
-        providerOptions, // required
-      });
-
-
-console.log(web3Modal);
-
-if (web3Modal.cachedProvider) { // continue directly to save time later
-  await onConnect();   
+// Initialize web3
+export async function initializeWeb3() {
+  web3 = new Web3(Web3.givenProvider); // web3->Web3
+  currentChainId = await web3.eth.net.getId();
+  let addressess = await web3.eth.getAccounts();
+  currentUserAddress = addressess[0];
 }
 
-
+export async function initializeContract(abi, contractAddress) {
+  if (!web3) throw 'Web3 not initialized';
+  let contractInstance = await new web3.eth.Contract(abi, contractAddress);
+  return contractInstance;
 }
 
-
-export function getWeb3Provider() {
-  return provider;
+export function getUserAddress() {
+  return currentUserAddress;
 }
 
 export function getWeb3() {
   return web3;
 }
 
-
-export function getUserAddress() {
- return  selectedAccount;
-
+function setChainId(chainId) {
+  currentChainId = chainId;
 }
 
-export async function Login() {
-    await onConnect();    
-}
-
-export async function authorize() {
-    await onConnect();
-}    
-
-/**
- * Connect wallet button pressed.
- */
- 
-var fOnlyOnce=false;
-async function onConnect() {
-    console.log("Opening a dialog", web3Modal);
-  
-    if (fOnlyOnce) return;
-    fOnlyOnce=true;
-  
-    try {
-        provider = await web3Modal.connect();
-    } catch(e) {
-        console.log("Could not get a wallet connection", e);
-        if (web3Modal)
-            web3Modal.clearCachedProvider(); // clear cached because this didn't work (try again later)
-        return;
-    }
-  
-  // Subscribe to accounts change
-  
-    try {
-        provider.on("accountsChanged", (accounts) => {
-            fetchAccountData();
-            publish("ethereumchanged")
-        });
-
-        // Subscribe to chainId change
-        provider.on("chainChanged", (chainId) => {
-            fetchAccountData();
-            publish("ethereumchanged")
-        });
-
-        // Subscribe to networkId change
-        provider.on("networkChanged", (networkId) => {
-            fetchAccountData();
-            publish("ethereumchanged")
-        });
-    } catch(e) {
-        console.log("provider on error", e);
-        return;
-    }
-  
-    web3 = new Web3(provider);
-
-    console.log("Web3 instance is", web3);
-
-  
-    await refreshAccountData();
-    console.log("web3providerfound");
-    publish("web3providerfound")
-
-    console.log(provider);
+function setUserAddress(userAddress) {
+  currentUserAddress = userAddress;
 }
