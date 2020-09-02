507e2ad84cd63647dc24d43110e7a515df21b2de
CryingEagle1 <m.gatsonides@hotmail.com>
2020-09-02 12:32:33 +0200

    Create koios_log.mjs

---
+++ koios_log.mjs
@@ -0,0 +1,44 @@
+import {DisplayMessageContinous,SwitchDisplayMessageContinous,DisplayMessage} from './koios_messages.mjs';
+
+var logtext;
+export function SetupLogWindow(fToHtml) {  // not async to
+    console.log("In SetupLogWindow");
+    logtext=document.createElement("pre"); // already create to be able to log
+    logtext.style.width = "100%";
+    logtext.style.height = "100%";   
+    logtext.style.fontSize="10px"
+    logtext.style.lineHeight="10px";
+  
+    var position=document.getElementById("log"); 
+    position.appendChild(logtext);    
+    log("Test logwindow")
+  
+ // logtext=document.getElementById("notes");
+ // console.log(logtext);
+  
+  if (fToHtml) {
+        console.log("Switching to HTML logging");
+        console.log=log;
+        console.error=log;
+        console.warn=log;
+  }
+    
+    
+    
+}
+export function log(s) {
+    //console.log(s);
+    //console.log(typeof s);  
+    if ((typeof s) !="string")  {
+        console.log("converting to string");
+        s = JSON.stringify(s);
+    }   
+        
+    if (logtext)
+        logtext.innerHTML +=s+"\r";
+    
+    logtext.scrollTop = logtext.scrollHeight; // keep the windows "scrolled down"
+    
+    
+    
+}
