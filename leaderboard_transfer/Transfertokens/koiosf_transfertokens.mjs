cba2ebbfe9c22f1fa9e27049e40ed392fc9371f8
Corwin <corwinvandalen@gmail.com>
2020-09-07 10:38:16 +0200

    Log test

--- leaderboard_transfer/Transfertokens/koiosf_transfertokens.mjs
+++ leaderboard_transfer/Transfertokens/koiosf_transfertokens.mjs
@@ -75,6 +75,7 @@ async function AddElementsToList() {
 }
 
 async function ShowAddresses(nameslist,addresses,tokenamount) {
+    console.log(addresses);
     if((nameslist.length == addresses.length) && (tokenamount.length == nameslist.length)) {    
         for (var i=0;i<addresses.length;i++) {
             if ((tokenamount[i] != 0) && ((addresses[i] != "NOG IN TE VULLEN ") || (addresses[i] != "x")) && ((addresses[i] != "") || (tokenamount[i] != "") || (nameslist[i] != ""))) { // Check for 0 and non address values
