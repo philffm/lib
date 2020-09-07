bfb016b81ecc6f9bdb5fb9fc05edefa322745bc5
Corwin <corwinvandalen@gmail.com>
2020-09-07 15:37:31 +0200

    Leaderboard test

--- leaderboard_transfer/Leaderboard/koiosf_leaderboard.mjs
+++ leaderboard_transfer/Leaderboard/koiosf_leaderboard.mjs
@@ -1,21 +1,90 @@
 import { } from "../../lib/3box.js"; // from "https://unpkg.com/3box/dist/3box.js"; // prevent rate errors
-
-import { getUserAddress, getWeb3Provider,authorize } from "../viewer_figma/koiosf_login.mjs";
-import {DomList,getElement,FitOneLine,LinkVisible,subscribe,GetImageIPFS} from '../lib/koiosf_util.mjs';
+import {DomList, getElement, subscribe, setElementVal, LinkClickButton, getElementVal, GetJson} from '../../lib/koiosf_util.mjs';
+import {DisplayMessage} from '../../viewer_figma/koiosf_messages.mjs';
+import {getUserAddress,getWeb3} from '../../viewer_figma/koiosf_login.mjs'
 
 var GlobalLeaderboardList = new DomList("leaderboardentry");
+var addresses = ["0x4373294dd0f98ec2783f759ae546a55e527487e7",
+                 "0x336101f6685906ffe861ac519a98a6736d2d5b37",
+                 "0x8e2a89ff2f45ed7f8c8506f846200d671e2f176f",
+                 "0xc3036b696ea52f6846f3f22e2eb709c595f0e09a",
+                 "0x4ad2eae4137e11eb3834840f1dc38f5f0fa181c3",
+                 "0x118df8e8557eb3ded10eb6fde3ef183ca8106d56",
+                 "0xea9a7c7cd8d4dc3acc6f0aaec1506c8d6041a1c5",
+                 "0xd4402f6ac4bdbdc3d966d1acab0c1c104ba7d89f",
+                 "0xbca3533d21a4538c43a4cc2a6f5c6d38271351c3",
+                 "0xe88cac4e10c4d316e0d52b82dd54f26ade3f0bb2",
+                 "0xb268b478f4b9e501daffce74da60cab8b7449871",
+                 "0x3d07b3629a319aabb2311c3f1f2ff048b1550bea",
+                 "0x5c84209877934c61047100121c70a4cf68ec270e"
+]
+var globalaccounts;
+var tokenfactoryJson;
+var tokenJson;
+var contracttokenfactory;
+var tokencount = new Array;
 
-async function ShowLeaderboard(participants) {
-    /*
-    for all participants:
-        if not yet in the list:
-            target = DomList of entries
-            leaderboardpositiontext = place in domlist
-            picture/name = FindProfile
-            tokencount = getTitanTokenCount(user address)
+window.addEventListener('DOMContentLoaded', onLoad)
 
-        Order DomList by TitanTokenCount
-    */
+async function onLoad() {
+    await initContractInformation();
+    await getTitanTokenCount();
+    await ShowLeaderboard(addresses, tokencount);
+}
+
+async function initContractInformation() {
+    subscribe("web3providerfound",NextStep)   
+    var tokenfactoryinfo="https://koiosonline.github.io/lib/koiosft/build/contracts/ERC20TokenFactory.json"
+	tokenfactoryJson=await GetJson(tokenfactoryinfo)
+	console.log(tokenfactoryinfo);
+	console.log(tokenfactoryJson)	
+	var tokensinfo="https://koiosonline.github.io/lib/koiosft/build/contracts/ERC20Token.json"
+	tokenJson=await GetJson(tokensinfo)
+	console.log(tokensinfo);
+	console.log(tokenJson)	
+}
+
+async function NextStep() {
+    console.log("In NextStep");
+    web3=getWeb3()
+    var nid=(await web3.eth.net.getId());
+    if (nid !=4) {
+        DisplayMessage(`Make sure you are on the Rinkeby network (currently ${nid})`);   
+    }    
+    globalaccounts = await web3.eth.getAccounts();
+	  	  
+    var tokenfactorycode=await web3.eth.getCode(tokenfactoryJson.networks[nid].address)
+    
+    if (tokenfactorycode.length <=2) {       
+        console.error("No contract code");        
+    } else {
+        contracttokenfactory = await new web3.eth.Contract(tokenfactoryJson.abi, tokenfactoryJson.networks[nid].address);
+        console.log(contracttokenfactory);
+    }
+}
+
+async function getTitanTokenCount() {
+    var totalTokens = await contracttokenfactory.methods.NrTokens().call();
+    for (var i=0;i<totalTokens;i++) {
+        var address=await contracttokenfactory.methods.tokens(i).call();
+        var contracttoken = await new web3.eth.Contract(tokenJson.abi, address);
+        var name = await contracttoken.methods.name().call();
+        if (name == "Titan") {
+            for (var i=0;i<addresses.length;i++) {      
+                tokencount[i] = await contracttoken.methods.balanceOf(addresses[i]).call();
+            }
+        }
+    }
+}
+
+async function ShowLeaderboard(addresses, tokencount) {
+    for (var i=0;i<addresses.length;i++) {
+        var target = GlobalLeaderboardList.AddListItem();
+        setElementVal("leaderboardtokencounttext",tokencount[i],target)
+        FindProfile(target.getElementsByClassName("leaderboardusertext")[0],addresses[i],target.getElementsByClassName("userphoto")[0]);
+        setElementVal("leaderboarduser",addresses[i],target)
+        setElementVal("leaderboardpositiontext",parseInt(i+1))
+    }
 }
 
 /*
