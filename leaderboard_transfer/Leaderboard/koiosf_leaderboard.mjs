dde5f18278d024f9bff58f2b18cdce34131d30a6
Corwin <corwinvandalen@gmail.com>
2020-09-08 13:17:00 +0200

    Test

--- leaderboard_transfer/Leaderboard/koiosf_leaderboard.mjs
+++ leaderboard_transfer/Leaderboard/koiosf_leaderboard.mjs
@@ -71,11 +71,11 @@ async function NextStep() {
 
 async function getTitanTokenCount() {
     var totalTokens = await contracttokenfactory.methods.NrTokens().call();
+    var addresspromises = new Array;
     for (var i=0;i<totalTokens;i++) {
             var address=await contracttokenfactory.methods.tokens(i).call();
             var contracttoken = await new web3.eth.Contract(tokenJson.abi, address);
             var name = await contracttoken.methods.name().call();
-            var addresspromises;
             if (name == "Titan") {
                 var decimals = await contracttoken.methods.decimals().call();
                 var nrOwners=await contracttoken.methods.nrOwners().call();
