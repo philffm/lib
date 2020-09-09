75295e751a2537b11f92794db700edeb7849a7bc
Corwin <corwinvandalen@gmail.com>
2020-09-09 11:37:55 +0200

    Test json filter

--- leaderboard_transfer/Leaderboard/koiosf_leaderboard.mjs
+++ leaderboard_transfer/Leaderboard/koiosf_leaderboard.mjs
@@ -5,27 +5,12 @@ import {getUserAddress,getWeb3} from '../../viewer_figma/koiosf_login.mjs'
 
 var GlobalLeaderboardList = new DomList("leaderboardentry");
 
-/*var nogoaddresses = ["0x4373294DD0f98eC2783f759Ae546A55E527487e7",
-                 "0x336101f6685906fFe861ac519A98A6736d2D5b37",
-                 "0x8e2A89fF2F45ed7f8C8506f846200D671e2f176f",
-                 "0xC3036b696Ea52F6846F3F22E2EB709C595F0e09A",
-                 "0x4Ad2eaE4137e11EB3834840f1DC38F5f0fa181c3",
-                 "0x118DF8e8557eB3DEd10Eb6Fde3EF183ca8106d56",
-                 "0xEA9a7c7cD8d4Dc3acc6f0AaEc1506C8D6041a1c5",
-                 "0xD4402f6AC4BdBdc3d966D1ACAB0c1c104bA7d89f",
-                 "0xbca3533d21a4538C43A4CC2A6f5c6D38271351C3",
-                 "0xe88cAc4e10C4D316E0d52B82dd54f26ade3f0Bb2",
-                 "0xB268B478F4B9e501dafFCe74dA60CAb8b7449871",
-                 "0x3d07b3629a319aabb2311c3f1f2ff048b1550bea",
-                 "0x5c84209877934c61047100121c70a4cf68ec270e",
-                 "0x0000000000000000000000000000000000000000",
-                 "0x59cDECe70FDd85E37546d9BE808Ae64892f1aD47",
-                 "0xC1b80113902f9cA05F159DAd4dDCD330E9d0F061"
-]*/
+var nonStudentAddresses = new Array;
 var globalaccounts;
 var tokenfactoryJson;
 var tokenJson;
 var contracttokenfactory;
+var nonStudentsJson;
 var tokencount = new Array;
 var addresses = new Array;
 var ranking = new Array;
@@ -45,7 +30,9 @@ async function initContractInformation() {
 	var tokensinfo="https://koiosonline.github.io/lib/koiosft/build/contracts/ERC20Token.json"
 	tokenJson=await GetJson(tokensinfo)
 	console.log(tokensinfo);
-	console.log(tokenJson)	
+    console.log(tokenJson)	
+    var nonStudents="https://koiosonline.github.io/lib/koiosft/migrations/distribute.json"
+    nonStudentsJson=await GetJson(nonStudents);
 }
 
 async function NextStep() {
@@ -64,6 +51,11 @@ async function NextStep() {
     } else {
         contracttokenfactory = await new web3.eth.Contract(tokenfactoryJson.abi, tokenfactoryJson.networks[nid].address);
         console.log(contracttokenfactory);
+    }
+
+    for (var i=0; i<nonStudentsJson.length; i++) {
+        nonStudentAddresses[i] = nonStudentsJson[i][0];
+        console.log(nonStudentAddresses);
     }
 
     await ShowLeaderboard();
@@ -111,7 +103,7 @@ async function ShowLeaderboard() {
     ranking.sort(function(a, b){return b[1]-a[1]});
     console.log(ranking);
     for (var i=0;i<10;i++) {
-        if ((ranking[i][1] != 0)) {
+        if ((ranking[i][1] != 0) && !(nonStudentAddresses.includes(ranking[i][0]))) {
             var target = GlobalLeaderboardList.AddListItem();
             setElementVal("leaderboardtokencounttext",ranking[i][1],target);
             FindProfile(target.getElementsByClassName("leaderboardusertext")[0],ranking[i][0],target.getElementsByClassName("userphoto")[0]);
