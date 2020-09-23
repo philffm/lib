fb21cdd16d3b1e8225c28c42e1d66e92cd985375
Corwin <corwinvandalen@gmail.com>
2020-09-23 14:33:25 +0200

    Test language loading

--- viewer_figma/koiosf_viewer.mjs
+++ viewer_figma/koiosf_viewer.mjs
@@ -385,6 +385,7 @@ subscribe('popupdisplaynone', x=> { if (fVideoRunning) startVideo(); } ); // if 
 subscribe("loadvideo",LoadVideo);
 
 async function LoadVideo(vidinfo) { // call when first video is loaded or a diffent video is selected
+    SetglobalplayerSubtitle(localStorage.getItem("currentlang"));
      if (!vidinfo) return;
     //console.log(`Loading video ${vidinfo.videoid} ${vidinfo.txt}`);
     //console.log(vidinfo);
